b6b301a999463c9fe2cdfa3846014f2ef4c89c1b
bob <marjabob910@gmail.com>
2021-02-12 12:50:18 +0100

    Coroutine1.3
    ~coroutine script

--- GPR year 2 semester 3/Assets/Scripts/Coroutine.cs
+++ GPR year 2 semester 3/Assets/Scripts/Coroutine.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class Coroutine : MonoBehaviour
 {
     [SerializeField] float _Seconds = 0.5f;
-
+    [SerializeField] Renderer _renderer;
 
     void Start()
     {
-        StartCoroutine(Routine());
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            StartCoroutine(Routine());
+        }
     }
 
     IEnumerator Routine()
     {
         Debug.Log("Ik start nu de coroutine");
-        yield return new WaitForSeconds(_Seconds);
-        for (float i = 9; i >= 0; i-- )
+        for (float i = 1; i >= 0; i-= 0.01f)
         {
-        Debug.Log("Coroutine update");
-        yield return new WaitForSeconds(_Seconds);
+            Color c = _renderer.material.color;
+            c.a = i;
+            _renderer.material.color = c;
+            Debug.Log("fade");
+            yield return new WaitForSeconds(_Seconds / 100);
         }
 
         Debug.Log("Coroutine einde");
