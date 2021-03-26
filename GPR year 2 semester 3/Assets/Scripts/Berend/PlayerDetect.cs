ed16421b2bc9f66b96808954de97023039ef79f6
Bob-Hoogenboom <55579218+Bob-Hoogenboom@users.noreply.github.com>
2021-03-26 13:30:08 +0100

    States (Berend)
    added player detection

---
+++ GPR year 2 semester 3/Assets/Scripts/Berend/PlayerDetect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetect : MonoBehaviour
+{
+    [SerializeField] private float _radius = 1f;
+
+    public Transform playerPos;
+
+
+    void Update()
+    {
+        PlayerDetectionSphere(_radius);
+    }
+
+    private void PlayerDetectionSphere(float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (var hitCollider in hitColliders)
+        {
+            Debug.Log(hitCollider);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+
+}
